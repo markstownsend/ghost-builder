3bb494aa829d3368c29856f64c6f4ed89144d00f
markstownsend <markstownsend@yahoo.com>
2015-07-01 16:56:29 +0100

    gulpfile for ico image

--- gulpfile.js
+++ gulpfile.js
@@ -15,15 +15,22 @@ gulp.task('replace', function(){
 		.pipe(replace(new RegExp('s.v=\\w+">','g'),function(match, p1, offset, string){
 			return 's"/>';
 		})) //removes the asset query string from an improperly shortcut closed tag i.e. <tag src="pathtosource?v=ghostquerystring"> note the missing back slash before the '>' 
-		.pipe(replace('</html>2368/assets/js/main.min.js"></script></body>','')) // some random string that showed up in the root index.html
+		.pipe(replace(new RegExp('<\/html>[\\s\\S]+<\/script>\\n<\/body>','i'), function(match, p1, offset, string){
+			return '';
+		})) // some random string that shows up in the foot of the root index.html
 		.pipe(replace('/assets/mark%20townsend%20technical%20lead.pdf','../assets/mark%20townsend%20technical%20lead.pdf')) // mangles the manual href for downloading a static doc
-		.pipe(replace('src="/public/jquery.js"','src="https://code.jquery.com/jquery-2.1.4.min.js"'))
-		.pipe(replace('/content/images/2015/06/','/assets/images/'))
+		.pipe(replace('src="/public/jquery.js"','src="https://code.jquery.com/jquery-2.1.4.min.js"')) // get jquery from the cdn
+		.pipe(replace(new RegExp('(\/content\/images\/20[0-9][0-9]\/[0|1][1-9]\/)(.+)','g'),function(match, p1, p2, offset, string){
+			return '/assets/images/' + p2;
+		})) // this is just putting all the images together under assets
+				.pipe(replace(new RegExp('..\/content\/images\/20[0-9][0-9]\/[0|1][1-9]\/.+','g'),function(match, p1, offset, string){
+			return '/assets/images/';
+		})) // same again but for paths from post sub-folders
 		.pipe(gulp.dest('./markstownsend.github.io'));
 });
 
 gulp.task('move-images', function(){
-	gulp.src(['static/**/*.jpg', 'static/**/*.png','../ghost-0.6.4/content/images/**/*.jpg','../ghost-0.6.4/content/images/**/*.png'])
+	gulp.src(['static/**/*.jpg', 'static/**/*.png','static/**/*.ico','../ghost-0.6.4/content/images/**/*.jpg','../ghost-0.6.4/content/images/**/*.png','../ghost-0.6.4/content/images/**/*.ico'])
 		.pipe(rename({dirname: ''}))
 		.pipe(gulp.dest('./markstownsend.github.io/assets/images'));
 });
