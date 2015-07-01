5687591ae67701258d657917b4475cad49b5fbf0
markstownsend <markstownsend@yahoo.com>
2015-07-01 17:12:18 +0100

    refined regexp for asset query string

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,15 @@ gulp.task('default', function() {
 
 gulp.task('replace', function(){
 	gulp.src('./static/**/*.html')
-		.pipe(replace(new RegExp('s.v=\\w+"><','g'),function(match, p1, offset, string){
-			return 's"><';
+		.pipe(replace(new RegExp('.v=\\w+"><','g'),function(match, p1, offset, string){
+			return '"><';
 		})) // removes the asset query string from a properly closed tag i.e. <tag src="pathtosource?v=ghostquerystring"></tag>
-		.pipe(replace(new RegExp('s.v=\\w+">','g'),function(match, p1, offset, string){
-			return 's"/>';
+		.pipe(replace(new RegExp('.v=\\w+">','g'),function(match, p1, offset, string){
+			return '"/>';
 		})) //removes the asset query string from an improperly shortcut closed tag i.e. <tag src="pathtosource?v=ghostquerystring"> note the missing back slash before the '>' 
 		.pipe(replace(new RegExp('<\/html>[\\s\\S]+<\/script>\\n<\/body>','i'), function(match, p1, offset, string){
 			return '';
-		})) // some random string that shows up in the foot of the root index.html
+		})) // some random string that shows up in the foot of the root index.html duplicating the body and html closing tags
 		.pipe(replace('/assets/mark%20townsend%20technical%20lead.pdf','../assets/mark%20townsend%20technical%20lead.pdf')) // mangles the manual href for downloading a static doc
 		.pipe(replace('src="/public/jquery.js"','src="https://code.jquery.com/jquery-2.1.4.min.js"')) // get jquery from the cdn
 		.pipe(replace(new RegExp('(\/content\/images\/20[0-9][0-9]\/[0|1][1-9]\/)(.+)','g'),function(match, p1, p2, offset, string){
